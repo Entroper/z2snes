0d496cd8b9484c1f04ae2bbf39a35ce2b3d2db7e
Entroper <entroper@gmail.com>
2023-01-31 16:13:52 -0500

    cleanup

--- src/main.asm
+++ src/main.asm
@@ -2,9 +2,6 @@
 .a8
 .i16
 
-.import JOYPAD1
-.import JOYTRIGGER1
-.import JOYHELD1
 .import GetJoypadInputs
 
 .import LoadOverworld
@@ -16,31 +13,27 @@
 .segment "CODE"
 
 .proc   ResetHandler
-        sei                     ; disable interrupts
-        clc                     ; clear the carry flag
-        xce                     ; switch the 65816 to native (16-bit mode)
-        rep #$10                ; set X and Y to 16-bit
-        sep #$20                ; set A to 8-bit
+	sei                     ; disable interrupts
+	clc                     ; clear the carry flag
+	xce                     ; switch the 65816 to native (16-bit mode)
+	rep #$10                ; set X and Y to 16-bit
+	sep #$20                ; set A to 8-bit
 
-		ldx #$1fff              ; set the stack pointer to $1fff
-        txs
+	ldx #$1fff              ; set the stack pointer to $1fff
+	txs
 
-		jsr LoadOverworld
+	jsr LoadOverworld
 
-        jmp GameLoop            ; all initialization is done
+	jmp GameLoop            ; all initialization is done
 .endproc
-;-------------------------------------------------------------------------------
 
-;-------------------------------------------------------------------------------
-;   Executed during V-Blank
-;-------------------------------------------------------------------------------
 .proc   GameLoop
-        wai                                 ; wait for NMI / V-Blank
+	wai                     ; wait for NMI / V-Blank
 
-		jsr GetJoypadInputs
-		jsr DoOverworldMovement
+	jsr GetJoypadInputs
+	jsr DoOverworldMovement
 
-        jmp GameLoop
+	jmp GameLoop
 .endproc
 
 .proc   NMIHandler
@@ -52,14 +45,14 @@
 .endproc
 
 .segment "VECTOR"
-; native mode   COP,        BRK,        ABT,
-.addr           $0000,      $0000,      $0000
-;               NMI,        RST,        IRQ
-.addr           NMIHandler, $0000,      $0000
+; native mode     COP,        BRK,          ABT,
+.addr             $0000,      $0000,        $0000
+;                 NMI,        RST,          IRQ
+.addr             NMIHandler, $0000,        $0000
 
-.word           $0000, $0000    ; four unused bytes
+.word             $0000, $0000    ; four unused bytes
 
-; emulation m.  COP,        BRK,        ABT,
-.addr           $0000,      $0000,      $0000
-;               NMI,        RST,        IRQ
-.addr           $0000,      ResetHandler, $0000
+; emulation mode  COP,        BRK,          ABT,
+.addr             $0000,      $0000,        $0000
+;                 NMI,        RST,          IRQ
+.addr             $0000,      ResetHandler, $0000
